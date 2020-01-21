9de3b1f44dc42585ac2361b93978790efa7b45d9
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-22 00:24:54 +0100

    wp-cli

---
+++ wp-cli/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wait-for-it wordpress:9000 -t 90 --strict -- \
+    wp core install \
+        --url=http://localhost:${WORDPRESS_PORT} \
+        --title=${WORDPRESS_TITLE} \
+        --admin_user=${WORDPRESS_LOGIN} \
+        --admin_password=${WORDPRESS_PASSWORD} \
+        --admin_email=${WORDPRESS_EMAIL} \
+        --skip-email \
+        \
+&& wp search-replace --regex \
+    "localhost:[0-9]*" \
+    "localhost:${WORDPRESS_PORT}"
