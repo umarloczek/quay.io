0e796373671acbb70d3c3e4fde4605d8f7d23cb6
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-23 06:37:06 +0100

    Create test_http.sh

---
+++ services/test_http.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eu
+source .env
+
+wait-for-url() {
+    echo "Testing $1"
+    timeout -s TERM 180 bash -c \
+    'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
+    do echo "Waiting for ${0}" && sleep 2;\
+    done' ${1}
+    echo "OK!"
+    curl -I $1
+}
+
+declare -r STATIC_PATH="/wp-includes/js/jquery/jquery.js"
+
+if [[ $1 == "develop" ]];
+then
+    declare -p WORDPRESS_PORT
+    declare -r HOST="localhost:${WORDPRESS_PORT}"
+fi
+
+if [[ $1 == "deploy" ]];
+then
+    declare -p DOMAIN_NAME
+    echo "127.0.0.1 $DOMAIN_NAME" | sudo tee -a /etc/hosts
+    declare -r HOST="${DOMAIN_NAME}"
+fi
+
+wait-for-url http://${HOST}
+wait-for-url http://${HOST}${STATIC_PATH}
+
+echo http://${HOST}
