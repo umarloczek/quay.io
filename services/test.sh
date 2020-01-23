c88dde87bf0b5d6358d2310bfae59793ba1e464b
Mateusz Kurowski <38087302+bukowa@users.noreply.github.com>
2020-01-23 06:42:19 +0100

    Update test.sh

--- services/test.sh
+++ services/test.sh
@@ -2,6 +2,14 @@
 set -eux
 source .env
 
+function develop (){
+    make up && ./test_http.sh develop
+}
+
+function deploy (){
+    make deploy && ./test_http.sh deploy
+}
+
 export WORDPRESS_PORT=3456
 develop
 deploy
@@ -11,10 +19,6 @@ develop
 make rm
 deploy
 
-function develop (){
-    make up && ./test_http.sh develop
-}
-
-function deploy (){
-    make deploy && ./test_http.sh deploy
-}
+export WORDPRESS_PORT=3457
+develop
+deploy
